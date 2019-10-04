dae5cf085fd5076c199958c8c214d82fbd2a657c
luiper79 <luiper79@users.noreply.github.com>
2019-10-04 15:10:24 -0500

    CommerceBank

---
+++ Source Code/CommerceBank1/CommerceBank/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CommerceBank.Models;
+
+namespace CommerceBank.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Description of Project Website Application .";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "UMKC Contact Information.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Source Code/CommerceBank1/CommerceBank/Data/CommerceBankContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CommerceBank.Models;
+
+namespace CommerceBank.Models
+{
+    public class CommerceBankContext : DbContext
+    {
+        public CommerceBankContext (DbContextOptions<CommerceBankContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CommerceBank.Models.Customer> Customer { get; set; }
+
+        public DbSet<CommerceBank.Models.Account> Account { get; set; }
+
+        public DbSet<CommerceBank.Models.Transaction> Transaction { get; set; }
+    }
+}
