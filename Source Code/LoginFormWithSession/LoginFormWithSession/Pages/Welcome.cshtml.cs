a8f2467ee2f4d20bb5b9e484cfc56c9c959231ae
luiper79 <luiper79@users.noreply.github.com>
2019-10-28 18:21:18 -0500

    SourceCode

---
+++ Source Code/LoginFormWithSession/LoginFormWithSession/Pages/Welcome.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginFormWithSession.Pages
+{
+    public class WelcomeModel : PageModel
+    {
+        public string Username;
+
+        public object UserHttpContext { get; private set; }
+
+        public void OnGet()
+        {
+            Username = HttpContext.Session.GetString("username");
+
+        }
+    }
+}
